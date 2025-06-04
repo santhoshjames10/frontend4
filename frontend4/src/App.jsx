c9835d36590cbd9d80cd0ff47dfd01342eb9d2e5
santhoshjames10 <santhoshjameslds@gmail.com>
2025-06-04 00:26:12 -1000

    add after adding all but few error in running

--- frontend4/src/App.jsx
+++ frontend4/src/App.jsx
@@ -1,21 +1,13 @@
-import "./css/App.css";
-import Favorites from "./pages/Favorites";
-import Home from "./pages/Home";
 import { Routes, Route } from "react-router-dom";
-import { MovieProvider } from "./contexts/MovieContext";
-import NavBar from "./components/NavBar";
+import Home from "./Pages/Home";
+import Favorites from "./Pages/Favorites";
 
 function App() {
   return (
-    <MovieProvider>
-      <NavBar />
-      <main className="main-content">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/favorites" element={<Favorites />} />
-        </Routes>
-      </main>
-    </MovieProvider>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/favorites" element={<Favorites />} />
+    </Routes>
   );
 }
 
